d19e1428ccd60c649ac0933e3239771557b78e58
Richard Green <shinyButterfly.Green@outlook.com>
2017-01-10 00:01:00 -0800

    Graph Data 2017-01-10T00:01

---
+++ myfile.sh
@@ -0,0 +1,4 @@
+date --rfc-3339='ns' > file.txt
+git add --all && GIT_AUTHOR_DATE='2017-01-10T00:01' GIT_COMMITTER_DATE='2017-01-10T00:01' git commit -m 'Graph Data 2017-01-10T00:01'
+date --rfc-3339='ns' > file.txt
+git add --all && GIT_AUTHOR_DATE='2017-01-10T00:01' GIT_COMMITTER_DATE='2017-01-10T00:01' git commit -m 'Graph Data 2017-01-10T00:01'
