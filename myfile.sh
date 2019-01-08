8183b049cf7e79343bd705c4472ed4395027b1ec
Richard Green <shinyButterfly.Green@outlook.com>
2019-01-08 00:01:00 -0800

    Graph Data 2019-01-08T00:01

--- myfile.sh
+++ myfile.sh
@@ -1,4 +1,6 @@
 date --rfc-3339='ns' > file.txt
-git add --all && GIT_AUTHOR_DATE='2017-01-10T00:01' GIT_COMMITTER_DATE='2017-01-10T00:01' git commit -m 'Graph Data 2017-01-10T00:01'
+git add --all && GIT_AUTHOR_DATE='2019-01-08T00:01' GIT_COMMITTER_DATE='2019-01-08T00:01' git commit -m 'Graph Data 2019-01-08T00:01'
 date --rfc-3339='ns' > file.txt
-git add --all && GIT_AUTHOR_DATE='2017-01-10T00:01' GIT_COMMITTER_DATE='2017-01-10T00:01' git commit -m 'Graph Data 2017-01-10T00:01'
+git add --all && GIT_AUTHOR_DATE='2019-01-08T00:01' GIT_COMMITTER_DATE='2019-01-08T00:01' git commit -m 'Graph Data 2019-01-08T00:01'
+date --rfc-3339='ns' > file.txt
+git add --all && GIT_AUTHOR_DATE='2019-01-08T00:01' GIT_COMMITTER_DATE='2019-01-08T00:01' git commit -m 'Graph Data 2019-01-08T00:01'
