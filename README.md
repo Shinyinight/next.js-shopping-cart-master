a6867ffe0e533fbee748ee2a052661ee06b1d725
Richard Green <iLikeCool03@gmail.com>
2023-01-18 19:44:34 -0800

    asdf

---
+++ README.md
@@ -0,0 +1,12 @@
+# Next.js Shopping Cart
+
+A Shopping cart built w/ Next.js, TypeScript, Chakra-ui
+
+## Tech Stack
+
+- NextJS
+- TypeScript
+- React-Query
+- Lodash (for debouncing)
+- Chakra-ui
+- React-icons
--- myfile.sh
+++
@@ -1,6 +0,0 @@
-date --rfc-3339='ns' > file.txt
-git add --all && GIT_AUTHOR_DATE='2019-01-08T00:01' GIT_COMMITTER_DATE='2019-01-08T00:01' git commit -m 'Graph Data 2019-01-08T00:01'
-date --rfc-3339='ns' > file.txt
-git add --all && GIT_AUTHOR_DATE='2019-01-08T00:01' GIT_COMMITTER_DATE='2019-01-08T00:01' git commit -m 'Graph Data 2019-01-08T00:01'
-date --rfc-3339='ns' > file.txt
-git add --all && GIT_AUTHOR_DATE='2019-01-08T00:01' GIT_COMMITTER_DATE='2019-01-08T00:01' git commit -m 'Graph Data 2019-01-08T00:01'
